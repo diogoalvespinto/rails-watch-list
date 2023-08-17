910a110ee52793c794e64a28c31fdae19dca7851
Diogo Pinto <diogo.alvespinto@gmail.com>
2023-08-17 13:24:21 +0100

    list controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,34 @@
 class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+
+    respond_to do |format|
+      if @list.save
+        format.html { redirect_to list_path(@list), notice: 'List was successfully created.' }
+        format.json { render :show, status: :created, location: @list }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @list.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
 end
